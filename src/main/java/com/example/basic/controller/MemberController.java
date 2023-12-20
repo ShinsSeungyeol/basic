a86889f709ee5a60ac15f052f2743e3c0d23dd7b
신승열 <shinseungyeol0227@gmail.com>
2023-12-20 21:29:40 +0900

    회원 등록 및 회원 조회

--- src/main/java/com/example/basic/controller/MemberController.java
+++ src/main/java/com/example/basic/controller/MemberController.java
@@ -1,8 +1,16 @@
 package com.example.basic.controller;
 
+import com.example.basic.domain.Member;
+import com.example.basic.domain.MemberForm;
 import com.example.basic.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +20,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
---
+++ src/main/java/com/example/basic/domain/MemberForm.java
@@ -0,0 +1,13 @@
+package com.example.basic.domain;
+
+public class MemberForm {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
