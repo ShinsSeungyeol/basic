da7c62e338281d07f9d4ba746cd66564108c5021
신승열 <shinseungyeol0227@gmail.com>
2023-12-20 22:18:09 +0900

    JdbcMemberRepository 구현

--- src/main/java/com/example/basic/SpringConfig.java
+++ src/main/java/com/example/basic/SpringConfig.java
@@ -1,14 +1,24 @@
 package com.example.basic;
 
+import com.example.basic.repository.JdbcMemberRepository;
 import com.example.basic.repository.MemberRepository;
 import com.example.basic.repository.MemoryMemberRepository;
 import com.example.basic.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource datasource;
+
+    @Autowired
+    public SpringConfig(DataSource datasource) {
+        this.datasource = datasource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -16,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(datasource);
     }
 }
