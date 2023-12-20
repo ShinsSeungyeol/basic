560b8deed87f2dcbe9cfb71a857f799f13438ef1
신승열 <shinseungyeol0227@gmail.com>
2023-12-20 17:48:40 +0900

    init_commit 컴포넌트 스캔

---
+++ src/main/java/com/example/basic/service/MemberService.java
@@ -0,0 +1,56 @@
+package com.example.basic.service;
+
+import com.example.basic.domain.Member;
+import com.example.basic.repository.MemberRepository;
+import com.example.basic.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        //Dependency Injection
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 멤버 검색
+     * @return
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 아이디로 회원 검색
+     * @param id
+     * @return
+     */
+    public Optional<Member> findOne(Long id) {
+        return memberRepository.findById(id);
+    }
+}
